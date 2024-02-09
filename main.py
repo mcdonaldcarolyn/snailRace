40b81a469ddd4428bb9f1cbbdfcf1e9af83ba4a8
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-02-09 19:27:22 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random, time, sys
+
+MAX_NUM_SNAILS = 8
+MAX_NAME_LENGTH = 20 
+FINISH_LINE = 40
+
+print(''' snail race by invent with python''')
+
+while True:
+    print('How many snails will Race? Max:', MAX_NUM_SNAILS)
+    
